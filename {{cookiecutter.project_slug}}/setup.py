96f1f444a1e19d6690cc56525ce89d0995caa458
Jean-Sebastien Dieu <dieu.jsebastien@yahoo.com>
2021-09-18 01:18:17 +0200

    Enforce str instead of int

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -55,7 +55,7 @@ setup(
         'Framework :: Pytest',
         'Intended Audience :: {{cookiecutter.intended_audience}}',
         'Programming Language :: Python',
-        'Programming Language :: Python :: {{cookiecutter.python_version_major}}',
+        'Programming Language :: Python :: ' + str("{{cookiecutter.python_version_major}}"),
         {%- for item in range(cookiecutter.python_version_min, cookiecutter.python_version_max + 1): %}
         'Programming Language :: Python :: {{ item }}',
         {%- endfor %}
